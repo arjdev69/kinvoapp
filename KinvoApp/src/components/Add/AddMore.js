c1f3966683c4c72538077af1663a08fdc0fe4eb0
jobino <jobino-araujo@hotmail.com>
2019-08-11 01:46:09 -0300

    add style

--- KinvoApp/src/components/Add/AddMore.js
+++ KinvoApp/src/components/Add/AddMore.js
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native'
 export default class AddMore extends Component {
     render() {
         return (
-            <View>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                 <Text> AddMore </Text>
             </View>
         )
--- KinvoApp/src/components/Premium/Premium.js
+++ KinvoApp/src/components/Premium/Premium.js
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native'
 export default class Premium extends Component {
     render() {
         return (
-            <View>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                 <Text> Premium </Text>
             </View>
         )
