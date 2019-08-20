fe94c46e0ac83e08ade52b32e2728899a032e2ab
arjdev69 <bruno96araujo@gmail.com>
2019-08-20 10:10:55 -0300

    resolve gesture handler

--- KinvoApp/react-native.config.js
+++ KinvoApp/react-native.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-    assets: ['./assets/fonts'], // stays the same
-  };
+  assets: ['./assets/fonts'], // stays the same
+  dependencies: {
+    '<dependency>': {
+      platforms: {
+        android: null, // disable Android platform, other platforms will still autolink
+      },
+    },
+  },
+};
